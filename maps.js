2cb7cff4f539c857ddbb7c54d76161b0a91193aa
Elana Kopelevich <ekopelevich@gmail.com>
2015-11-11 18:38:42 -0700

    added pin

--- maps.js
+++ maps.js
@@ -1,13 +1,49 @@
 function initialize() {
-  console.log('something');
+
   var mapCanvas = document.getElementById('map');
   var mapOptions = {
       center: new google.maps.LatLng(39.7675, -105.0200),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+    };
+
     var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    placePin(map);
+
+    console.log(google.maps.LatLng);
 }
+
+function placePin(map) {
+  var myLatLng = {lat: 39.7675, lng: -105.0200};
+
+  var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  console.log(marker.position);
+}
+
+function testAddress() {
+  var pinAddress = $.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YAIzaSyBoXK03DpDGwohDBYYR2CdtSYnr4W0H5no');
+  console.log(pinAddress);
+}
+
+// function geocodeAddress(geocoder, resultsMap) {
+//   address = $('#address').value;
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === google.maps.GeocoderStatus.OK) {
+//       resultsMap.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//         map: resultsMap,
+//         position: results[0].geometry.location
+//       });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
 
 function eventListener() {
   var address;
@@ -18,25 +54,25 @@ function eventListener() {
    });
 }
 
+// function formSubmit() {
+//     $('#address-search').submit(submit);
+// }
+
 // function submit(n) {
-//   var text = $("#input").val();
-//   console.log(text);
-//
+//   var address = $('#address').val();
+//   console.log(address);
+
 //   preventDefault();
 //   return false;
-// };
+// }
 
 function init() {
-    eventListener();
+  initialize();
+  testAddress();
+  eventListener();
 }
 
 $( function() {
-    console.log( "ready!" );
-    init();
+  console.log( "ready!" );
+  init();
 });
-
-// function formSubmit() {
-//     $('#form').submit(submit);
-// };
-
-google.maps.event.addDomListener(window, 'load', initialize);
